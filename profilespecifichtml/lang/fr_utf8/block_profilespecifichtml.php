633caf3209e98d07bb32bf45a0d06ec41c1e6227
Valery Fremaux <valery.fremaux@gmail.com>
2012-06-09 19:39:35 +0200

    Moodle v1.9 code load

---
+++ profilespecifichtml/lang/fr_utf8/block_profilespecifichtml.php
@@ -0,0 +1,18 @@
+<?php
+
+$string['blockname'] = 'HTML (par profil)';
+$string['newhtmlblock'] = 'Nouveau bloc Html sur profil';
+$string['leaveblanktohide'] = 'Laisser vide pour cacher';
+$string['configtitle'] = 'Titre visible du bloc ';
+$string['configprofilefield1'] = 'Champ 1 ';
+$string['configprofilefield2'] = 'Champ 2 ';
+$string['configprofilevalue1'] = 'Valeur 1 ';
+$string['configprofilevalue2'] = 'Valeur 2 ';
+$string['configprofileop'] = 'Operation ';
+$string['-'] = '(ignorer)';
+$string['and'] = 'ET';
+$string['or'] = 'OU';
+$string['xor'] = 'OU ALORS';
+$string['configcontentwhenmatch'] = 'Contenu si conforme ';
+$string['configcontentwhennomatch'] = 'Contenu si non conforme ';
+$string['configcontentforall'] = 'Contenu pour tous';
